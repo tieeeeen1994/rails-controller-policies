a77c548125cc2750327005b09c6c05554a913155
Tien <tieeeeen1994@gmail.com>
2024-04-12 17:24:32 +0800

    Add a way for has_enforced_policies to accept a block

--- lib/controller_policies/action_controller_patch.rb
+++ lib/controller_policies/action_controller_patch.rb
@@ -3,8 +3,14 @@
 module ControllerPolicies
   # Module containing action controller patches.
   module ActionControllerPatch
-    def has_enforced_policies # rubocop:disable Naming/PredicateName
+    def has_enforced_policies(&block) # rubocop:disable Naming/PredicateName
       include ControllerPolicies::Enforcement
+
+      return unless block_given?
+
+      define_method :ability? do |ability_code|
+        block.call(ability_code)
+      end
     end
   end
 end
