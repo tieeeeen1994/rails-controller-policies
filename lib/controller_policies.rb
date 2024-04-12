57ad8f060983ddd249a6307c32c91448f8d69b0c
Tien <tieeeeen1994@gmail.com>
2024-04-12 16:50:22 +0800

    require ability

--- lib/controller_policies.rb
+++ lib/controller_policies.rb
@@ -4,6 +4,8 @@ require_relative 'controller_policies/version'
 
 raise LoadError, 'The project is not a Rails project!' unless defined?(Rails)
 
+require_relative 'ability'
+
 # Base namespace for the gem.
 module ControllerPolicies
   # Exception raised for unimplemented ability? method for those including ControllerPolicies::Enforcement.
